13429cfab5a96c27feea725785587f838e07ca0a
JesusMartinezLopez <jesusmartinezafellay@gmail.com>
2024-02-12 20:24:51 +0100

    Create calculator with styles and first functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-//
+const screen = document.getElementById('screen');
+
+window.onload = () => {
+  screen.value = 0
+}
+
+const addNumber = (number) => {
+  if (screen.value == 0) {
+    screen.value = number;
+  } else {
+    screen.value = screen.value + number;
+  }
+  
+}
+
+const clear = () => {
+
+}
+
+const del = () => {
+  let number = screen.value;
+  if (number.length < 2) {
+    screen.value = 0;
+  } else {
+    let newNumber = number.substring(0, number.length - 1)
+    screen.value = newNumber;
+  }
+}
